12bee2739068937b991a5f1d3e448c8dad6f5075
Natallia Viazenkina <natala.viaz@gmail.com>
2020-08-24 21:10:04 +0300

    Оживляет аккордеон

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,11 @@
 import {polyfills} from './utils/polyfills';
+import accordion from './modules/accordion';
 // Utils
 // ---------------------------------
 
 polyfills();
+
+// Modules
+// -----------------------------------
+accordion();
 
---
+++ source/js/modules/accordion.js
@@ -0,0 +1,63 @@
+export default () => {
+  const accordion = document.querySelector('.accordion');
+  if (!accordion) {
+    return;
+  }
+
+  const keyCodeEnter = 13;
+  let currentOpenedTab;
+  let currentOpenedTabContent;
+
+  function onTitleClick(evt) {
+    const tab = evt.target.closest('.accordion__tab');
+    if (!tab) {
+      return;
+    }
+    const tabContent = tab.querySelector('.accordion__tab-content');
+    if (!tabContent) {
+      return;
+    }
+
+    const tabContentHeight = tabContent.scrollHeight + 'px';
+
+    if (!tab.classList.contains('accordion__tab--opened')) {
+      if (currentOpenedTab) {
+        currentOpenedTab.classList.remove('accordion__tab--opened');
+        currentOpenedTabContent.style.height = 0;
+      }
+      currentOpenedTab = tab;
+      currentOpenedTabContent = tabContent;
+      currentOpenedTab.classList.add('accordion__tab--opened');
+    } else {
+      currentOpenedTab.classList.remove('accordion__tab--opened');
+      currentOpenedTab = null;
+      currentOpenedTabContent = null;
+      document.activeElement.blur();
+    }
+
+    tabContent.style.height = (tabContent.offsetHeight === 0) ? tabContentHeight : 0;
+
+
+  }
+
+  function onDocumentKeyDown(evt) {
+    if (evt.keyCode === keyCodeEnter && evt.target.classList.contains('accordion__tab-title')) {
+      evt.preventDefault();
+      onTitleClick(evt);
+    }
+  }
+
+  function initializeAccordion() {
+    const toggleList = accordion.querySelectorAll('.accordion__tab-toggle');
+    if (!toggleList.length) {
+      return;
+    }
+
+    toggleList.forEach((toggle) => {
+      toggle.addEventListener('click', onTitleClick);
+    });
+  }
+
+  initializeAccordion();
+  document.addEventListener('keydown', onDocumentKeyDown);
+};
